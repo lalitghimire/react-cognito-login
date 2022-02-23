0b7584a0dc5b04b1d0ff49cc87a562600ccff951
Lalit Ghimire <lalitghimire@gmail.com>
2022-02-23 10:16:30 +0200

    add forgot password form component

--- src/components/ForgetPassword.js
+++ src/components/ForgetPassword.js
@@ -1,9 +1,50 @@
-import React from 'react';
-//import { CognitoUser, AuthenticationDetails } from 'amazon-cognito-identity-js';
-//import Userpool from '../Userpool';
+import React, { useState } from 'react';
+import { CognitoUser } from 'amazon-cognito-identity-js';
+import Pool from '../Userpool';
 
 const ForgetPassword = () => {
-    return <div>Forget Password</div>;
+    const [email, setEmail] = useState('');
+    const [msg, setMsg] = useState('');
+
+    const getUser = () => {
+        return new CognitoUser({
+            Username: email,
+            Pool,
+        });
+    };
+
+    const sendVerificationCode = (e) => {
+        e.preventDefault();
+        getUser().forgotPassword({
+            onSuccess: (data) => {
+                console.log('onSucess: ', data);
+                setMsg('Verification code sent to your email');
+            },
+            onFailure: (err) => {
+                console.error('onFailure: ', err);
+                setMsg(`${err}`);
+            },
+            inputVerificationCode: (data) => {
+                console.log('verification code: ', data);
+            },
+        });
+    };
+
+    return (
+        <div>
+            {msg && <p>{msg}</p>}
+            <form onSubmit={sendVerificationCode}>
+                <input
+                    value={email}
+                    onChange={(e) => {
+                        setEmail(e.target.value);
+                    }}
+                />
+
+                <button type='submit'>Send Verification code</button>
+            </form>
+        </div>
+    );
 };
 
 export default ForgetPassword;
